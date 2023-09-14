7315747e2e297b4f3cfa57dc8d91cd3449b36c1e
bartek3xe <bartek3xe@gmail.com>
2023-09-15 00:01:42 +0200

    Create Cache handler

---
+++ src/NBP/Service/CacheHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\NBP\Service;
+
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+class CacheHandler
+{
+    private FilesystemAdapter $cache;
+
+    public function __construct()
+    {
+        $this->cache = new FilesystemAdapter();
+    }
+
+    public function setCacheDirectory(string $cacheDirectory): void
+    {
+        $this->cache = new FilesystemAdapter('', 0, $cacheDirectory);
+    }
+
+    public function getOrSet(string $cacheKey, callable $valueCallback, int $ttl = 3600): mixed
+    {
+        $cachedResult = $this->cache->getItem($cacheKey);
+
+        if (!$cachedResult->isHit()) {
+            $result = $valueCallback();
+
+            $cachedResult->set($result);
+            $cachedResult->expiresAfter($ttl);
+
+            $this->cache->save($cachedResult);
+        } else {
+            $result = $cachedResult->get();
+        }
+
+        return $result;
+    }
+
+    public function clear(): void
+    {
+        $this->cache->clear();
+    }
+}
