e0546f4dd0a9be69f31ffc7be046f471ed3827e1
bartek3xe <bartek3xe@gmail.com>
2023-09-14 22:29:07 +0200

    Remove unnecessary webpack-encore-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
